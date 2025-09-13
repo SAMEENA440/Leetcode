7f401e20a7b593622de63dfd2d7ad2fa6dae8fdd
SAMEENA440 <167673189+SAMEENA440@users.noreply.github.com>
2025-09-13 09:21:47 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (99.55%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -1,18 +1,44 @@
+// class Solution {
+//     public int maxFreqSum(String s) {
+//         Map<Character, Integer> freq = new HashMap<>();
+//         int con = 0, vow = 0;
+//         for (char c : s.toCharArray()) {
+//             freq.put(c, freq.getOrDefault(c, 0) + 1);
+//         }
+//         for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+//             char ch = entry.getKey();
+//             int count = entry.getValue();
+//             if ("aeiou".indexOf(ch) >= 0)
+//                 vow = Math.max(vow, count);
+//             else
+//                 con = Math.max(con, count);
+//         }
+//         return con + vow;
+//     }
+// }
+
+
+
+
 class Solution {
     public int maxFreqSum(String s) {
-        Map<Character, Integer> freq = new HashMap<>();
-        int con = 0, vow = 0;
-        for (char c : s.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
-        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
-            char ch = entry.getKey();
-            int count = entry.getValue();
-            if ("aeiou".indexOf(ch) >= 0)
-                vow = Math.max(vow, count);
-            else
-                con = Math.max(con, count);
+        String vowels = "aeiou";
+        int maxVowel = 0, maxConsonant = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                char c = (char)(i + 'a');
+                if (vowels.indexOf(c) != -1) {
+                    maxVowel = Math.max(maxVowel, freq[i]);
+                } else {
+                    maxConsonant = Math.max(maxConsonant, freq[i]);
+                }
+            }
         }
-        return con + vow;
+
+        return maxVowel + maxConsonant;
     }
 }
